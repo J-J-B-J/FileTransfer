43c8169de046eeeb678e69da13b1a4378ffe7461
J-J-B-J <79617327+J-J-B-J@users.noreply.github.com>
2024-01-24 08:23:04 +1100

    Same-name send and receive Transfer the file and keep the name of it the same.

--- RecieveFile.py
+++ RecieveFile.py
@@ -1,6 +1,6 @@
 """Receive a file over the socket"""
 import socket
-from tkinter.filedialog import asksaveasfile
+from tkinter.filedialog import askdirectory
 
 code = input("Enter sender code to receive file: ").split("-")
 HOST = code[0]
@@ -8,14 +8,21 @@ PORT = code[1]
 del code
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Connect
     s.connect((HOST, int(PORT)))
     print(f"Connected to {HOST}")
 
+    # Receive name
+    name = s.recv(4096)
+
+    # Receive data
     lines = []
     data = ""
     while data != b'':
         data = s.recv(4096)
         lines.append(data)
 
-with asksaveasfile("wb") as f:
+# Save the file
+file_name = askdirectory() + "/" + name.decode()
+with open(file_name, "wb") as f:
     f.writelines(lines)
--- Sendfile.py
+++ Sendfile.py
@@ -1,20 +1,28 @@
 """Send a file over the socket"""
 import socket
-from tkinter.filedialog import askopenfile
+from tkinter.filedialog import askopenfilename
 
 # Ask for the file
-with askopenfile(mode='rb') as f:
+file_name = askopenfilename()
+with open(file_name, 'rb') as f:
     lines = f.readlines()
 
 # Send the file
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Bind
     s.bind(("", 0))
 
     print("Ready to send!")
     print(f"Your send code: {socket.gethostname()}-{s.getsockname()[1]}")
 
+    # Listen and accept connection
     s.listen()
     conn, addr = s.accept()
+
+    # Send the name
+    conn.sendall(file_name.split("/")[-1].encode())
+
+    # Send the data
     with conn:
         print(f"Connected to {addr}")
         for line in lines:
